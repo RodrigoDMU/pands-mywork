38107faada851686f2dfc12a7d9c47baae85bb3e
RodrigoRDMU <rdm.demartino@gmail.com>
2024-02-01 23:50:13 +0000

    Week 02

---
+++ bank.py
@@ -0,0 +1,12 @@
+# Weekly Tasks 02
+
+# Prompt the user and read in two money amounts (in cent)
+# Add the two amounts
+# Print out the answer in a human readable format with a euro sign and decimal point between the euro and cent of the amount 
+
+# Author Rodrigo De Martino Ucedo
+
+amount1 = int(input('Enter amount (in cent): '))
+amount2 = int(input('Enter amount (in cent): '))
+sum = amount1 / 100 + amount2 / 100
+print(f'The sum of these is €{sum}')
