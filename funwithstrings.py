f128a3bcf814da6edeb5b6278ec76a6ea9a9944d
RodrigoRDMU <rdm.demartino@gmail.com>
2024-02-06 23:50:26 +0000

    Week 03

---
+++ funwithstrings.py
@@ -0,0 +1,17 @@
+# This is just me messing with strings
+# showing:
+#   Escape characters
+#   Adding numbers to strings
+#   Splicing
+
+# Author: Rodrigo De Martino Ucedo
+
+astring = "He said \"hello\" "
+print (astring)
+
+mark = 33
+string2 = "Your mark is: " + str(mark)
+print (string2)
+
+accountno = "123456789"
+print (accountno[0:4])
---
+++ messingwithnumbers.py
@@ -0,0 +1,9 @@
+# Messing with numbers
+
+# Author: Rodrigo De Martino Ucedo
+
+afloat = .29
+ascent = afloat * 100
+
+print (f"{afloat} as cent is {ascent}")
+
